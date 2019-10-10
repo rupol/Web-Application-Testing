aa5f38a782e21cd01dda656c55adb28b801871b8
Ruth Poliakon <r.poliakon@gmail.com>
2019-10-10 17:55:46 -0600

    basic functionality and styling

--- baseball-scoreboard/src/App.js
+++ baseball-scoreboard/src/App.js
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [balls, setBalls] = useState(0);
+  const [strikes, setStrikes] = useState(0);
+  const [hits, setHits] = useState(0);
+
+  const ballSetter = event => {
+    if (balls < 3 && strikes < 3) {
+      setBalls(balls + 1);
+    } else {
+      setBalls(0);
+      setStrikes(0);
+    }
+  };
+
+  const strikeSetter = event => {
+    if (balls < 4 && strikes < 2) {
+      setStrikes(strikes + 1);
+    } else {
+      setBalls(0);
+      setStrikes(0);
+    }
+  };
+
+  const hitSetter = event => {
+    setBalls(0);
+    setStrikes(0);
+  };
+
+  const foulSetter = event => {
+    if (strikes < 2) {
+      setStrikes(strikes + 1);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Scoreboard</h1>
+
+      <div className="scoreboard">
+        <div>
+          <h2>Balls:</h2>
+          <h3>{balls}</h3>
+        </div>
+
+        <div>
+          <h2>Strikes:</h2>
+          <h3>{strikes}</h3>
+        </div>
+      </div>
+
+      <div className="dashboard">
+        <button onClick={ballSetter}>Ball</button>
+        <button onClick={strikeSetter}>Strike</button>
+        <button onClick={hitSetter}>Hit</button>
+        <button onClick={foulSetter}>Foul</button>
+      </div>
     </div>
   );
 }
